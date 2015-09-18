af325902713a6e6b453a16673c15d12d2c53f2d7
osamagucCitySoc <osamarabie@Osamas-MacBook-Pro.local>
2015-09-18 17:37:40 +0300

    Initial Commit

---
+++ BrandsOnFire/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BrandsOnFire
+//
+//  Created by Osama Rabie on 9/18/15.
+//  Copyright (c) 2015 Osama Rabie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
